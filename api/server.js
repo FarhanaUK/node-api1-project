ce63c5ca6af050bd91b2357dadd45cdaace92b0f
Farhana Aktar <farhanaaktar@live.co.uk>
2024-10-28 12:24:36 +0000

    get by Id passed

--- api/server.js
+++ api/server.js
@@ -4,41 +4,39 @@ const User = require('./users/model')
 
 const server = express()
 
-server.use(express.json())
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+     res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users', 
+            err: err.message,
+            stack: err.stack,
+
+        })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+User.findById(req.params.id)
+    .then(users => {
+      res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'getting error',
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+})
+
 
 server.use('*', (req, res)=> {
     res.status(404).json({
         message: 'not found'
     })
 })
-
-server.get('/api/users', async (req, res) => {
-
-try{
-const users = await User.find()
-console.log(users)
-res.status(200).json(users)
-}
-catch(err){
-    res.status(500).json({ message: `Something Horrible ${err.message}` })
-
-}
-})
-
-server.get('/api/users/:id', (req, res) => {
-    User.findById(req.params.id)
-    .then(user => {
-       if(!user) {
-res.status(404).json({message: "the user does not exist"})
-       }
-      
-    })
-    .catch(err => {
-        res.status(500).json({
-         message: 'Error fetching',
-         err: err.message, 
-         stack: err.stack })
-    })
-})
-
 module.exports = server // EXPORT YOUR SERVER instead of {}
