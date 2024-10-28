e00b637473e2ecbea5f6479646409503e2713221
Farhana Aktar <farhanaaktar@live.co.uk>
2024-10-28 11:51:34 +0000

    get test passed

--- api/server.js
+++ api/server.js
@@ -1,3 +1,44 @@
 // BUILD YOUR SERVER HERE
+const express = require("express")
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+server.use('*', (req, res)=> {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+server.get('/api/users', async (req, res) => {
+
+try{
+const users = await User.find()
+console.log(users)
+res.status(200).json(users)
+}
+catch(err){
+    res.status(500).json({ message: `Something Horrible ${err.message}` })
+
+}
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+       if(!user) {
+res.status(404).json({message: "the user does not exist"})
+       }
+      
+    })
+    .catch(err => {
+        res.status(500).json({
+         message: 'Error fetching',
+         err: err.message, 
+         stack: err.stack })
+    })
+})
+
+module.exports = server // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -2,4 +2,13 @@ const server = require('./api/server');
 
 const port = 9000;
 
-// START YOUR SERVER HERE
+const express = require("express")
+
+server.get('/api/users', async (req, res) => {
+
+})
+
+server.listen(port, () => {
+
+    console.log(`server start on ${port}`)
+})
